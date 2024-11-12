be4448ef38726451133d7b9e81b448f5a3eb00b4
Riru312 <suyan4321@gmail.com>
2024-11-11 23:02:21 -0600

    adding marker

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function initMap() {
-	var map;
-	map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+	var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 64.073082, lng: -141.937393 },
     zoom: 8,
   });
+	var marker = new google.maps.Marker({
+		position: { lat: 64.073082, lng: -141.937393 },
+	})
 }
