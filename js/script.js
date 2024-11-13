c56325e980237a81616b5214e024a464e43b0775
Riru312 <suyan4321@gmail.com>
2024-11-12 23:25:40 -0600

    adding new map things

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ function initMap() {
     center: { lat: 64.073082, lng: -141.937393 },
     zoom: 8,
   });
+
 	var marker = new google.maps.Marker({
 		position: { lat: 64.073082, lng: -141.937393 },
-		map: map,
-		title: "dont click",
-		gmpClickable: true
+		map: map
 	});
+
+	const imageBounds = {
+    north: (64.073082 + 0.1),
+    south: (64.073082 - 0.1),
+    east: (-141.937393 + 0.1),
+    west: (-141.937393 - 0.1),
+  };
+
+  var chicken = new google.maps.GroundOverlay(
+    "images/jumpscare.jpeg",
+    imageBounds,
+  );
+  chicken.setMap(map);
 }
