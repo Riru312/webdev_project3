1d615e5b8df0048e917da0d7b218d43c1c46db27
Riru312 <suyan4321@gmail.com>
2024-11-12 23:46:58 -0600

    changing zoom

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
 	var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 64.073082, lng: -141.937393 },
-    zoom: 8,
+    zoom: 10,
   });
 
 	var marker = new google.maps.Marker({
