2f615bb326c819fc11ff7a60d2ea888bae8792b6
Riru312 <suyan4321@gmail.com>
2024-11-14 23:53:13 -0600

    fixing slider

--- js/script.js
+++ js/script.js
@@ -51,35 +51,3 @@ function initMap() {
 
   flightPath2.setMap(map);
 }
-
-$(document).ready(function () {
-         let slideCount = $('.slide').length;
-         let currentIndex = 0;
-         $('.slide').hide();
-         $('.active').show();
-         $('.prev').on('click', function () {
-            currentIndex--;
-            if (currentIndex < 0) {
-               currentIndex = slideCount - 1;
-            }
-            showSlide(currentIndex);
-         });
-         $('.next').on('click', function () {
-            currentIndex++;
-            if (currentIndex >= slideCount) {
-               currentIndex = 0;
-            }
-            showSlide(currentIndex);
-         });
-         $('.dot').on('click', function () {
-            currentIndex = $(this).index();
-            showSlide(currentIndex);
-         });
-         function showSlide(index) {
-            $('.slide').hide().removeClass('active');
-            $('.dot').removeClass('active-dot');
-            $('.slide').eq(index).show().addClass('active');
-            $('.dot').eq(index).addClass('active-dot');
-         }
-         showSlide(currentIndex);
-      });
