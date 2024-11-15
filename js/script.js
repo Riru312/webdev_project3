0c73ba142d9680c98676d733a133056f300fc301
Riru312 <suyan4321@gmail.com>
2024-11-14 23:35:00 -0600

    fixing slider

--- js/script.js
+++ js/script.js
@@ -52,43 +52,13 @@ function initMap() {
   flightPath2.setMap(map);
 }
 
-let slideIndex = 1;
-displaySlide(slideIndex);
- 
-function moveSlides(n) {
-    displaySlide(slideIndex += n);
-}
- 
-function activeSlide(n) {
-    displaySlide(slideIndex = n);
-}
- 
-/* Main function */
-function displaySlide(n) {
-    let i;
-    let totalslides =
-        document.getElementsByClassName("slide");
-    let totaldots =
-        document.getElementsByClassName("footerdot");
-				
-    totalslides[1].style.display = "block";
-    totaldots[1].className += " active";
-		slideIndex = 1;
-		
-    if (n > totalslides.length) {
-        slideIndex = 1;
-    }
- 
-    if (n < 1) {
-        slideIndex = totalslides.length;
-    }
-    for (i = 0; i < totalslides.length; i++) {
-        totalslides[i].style.display = "none";
-    }
-    for (i = 0; i < totaldots.length; i++) {
-        totaldots[i].className =
-            totaldots[i].className.replace(" active", "");
-    }
-    totalslides[slideIndex - 1].style.display = "block";
-    totaldots[slideIndex - 1].className += " active";
-}
+$(window).load(function() {
+        $('#slider').nivoSlider({
+            effect: 'random',
+            directionNavHide: false,
+            pauseOnHover: true,
+            captionOpacity: 1,
+            prevText: '<',
+            nextText: '>'
+  });
+});
